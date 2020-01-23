4b43f72a9e645a16d2b514e52d6e8092988b8bab
alxppgithub <alxppgithub@protonmail.ch>
2020-01-23 13:19:01 +0100

    initial commit

---
+++ entrypoint.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+echo $GITHUB_REPOSITORY
+
+realActor=""
+realRepo=""
+
+while IFS='/' read -ra ADDR; do
+	for i in "${ADDR[@]}"; do
+		# process "$i"
+		realActor="${ADDR[0]}"
+		realRepo="${ADDR[1]}"
+	done
+done <<< $GITHUB_REPOSITORY
+ 
+echo $realActor
+echo $realRepo
+
+git clone https://github.com/$realActor/$realRepo.git
+buildExist="$(cd $realRepo && git tag | grep build)"
+
+tag=""
+if [[ $buildExist ]]; then
+    echo "buildnr increment"
+	lastestBuildNr="$(cd $realRepo && git tag | grep build | sort -V -r | head -n1 | cut -c 7-)"
+	echo $lastestBuildNr
+	lastestBuildNr=$((lastestBuildNr+1))
+	echo $lastestBuildNr
+	tag="build-${lastestBuildNr}"
+	echo $tag
+else
+    echo "no buildnr"
+	tag="build-1"
+fi
+
+cd $realRepo
+git remote set-url --push origin https://$realActor:$GITHUB_TOKEN@github.com/$realActor/$realRepo
+git tag $tag
+
+git push origin $tag
