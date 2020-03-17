481cafc4edaa58621b9dc3a6b03742d1ce4e0edd
pplxgit <pplxgit@protonmail.ch>
2020-03-17 09:13:04 +0100

    fixed cut command

--- entrypoint.sh
+++ entrypoint.sh
@@ -38,7 +38,7 @@ tag=""
 if [[ $buildExist ]]; then
 	echo "buildnr increment"
 	if [[ $GITHUB_REF == *dev ]]; then
-		lastestBuildNr="$(cd $realRepo && git tag | grep dev | sort -V -r | head -n1 | cut -c 7-)"
+		lastestBuildNr="$(cd $realRepo && git tag | grep dev | sort -V -r | head -n1 | cut -c 5-)"
 		echo $lastestBuildNr
 		lastestBuildNr=$((lastestBuildNr+1))
 		echo $lastestBuildNr
